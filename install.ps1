0fb11aaad5b70d4c8250a95934fc803b4590d3e4
Ciro Diaz <161502824+CiroZDP@users.noreply.github.com>
2024-09-24 21:29:51 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,11 @@
-Start-Process "https://github.com/OpenCraft-Studios/OpenCraft/actions/runs/10983895248/artifacts/1963680486"
+# Create opencraft directory
+New-Item -Path "opencraft" -ItemType "Directory"
+
+# Change directory
+Set-Location "opencraft"
+
+# Download file opencraft.jar
+Invoke-WebRequest -Uri "https://github.com/OpenCraft-Studios/OpenCraft/releases/download/auto-release/opencraft.jar" -OutFile "opencraft.jar"
+
+# Execute it
+java -jar "opencraft.jar"
