41b01ddc1a1c46a89548d4d1e1ff60ebb0fdaff1
Ciro Diaz <161502824+CiroZDP@users.noreply.github.com>
2024-09-25 20:31:08 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,11 +1,30 @@
-# Create opencraft directory
-New-Item -Path "opencraft" -ItemType "Directory"
+# === Download WGET ===
+  Write-Host -NoNewLine -ForegroundColor "white" " >  "
+  Write-Host            -ForegroundColor "green" "Downloading WGET...."
 
-# Change directory
-Set-Location "opencraft"
+  Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.21.4/32/wget.exe" -OutFile ".\wget.exe"
 
-# Download file opencraft.jar
-Invoke-WebRequest -Uri "https://github.com/OpenCraft-Studios/OpenCraft/releases/download/auto-release/opencraft.jar" -OutFile "opencraft.jar"
+  Write-Host -NoNewLine -ForegroundColor "white" " >  "
+  Write-Host            -ForegroundColor "green" "Done!"
 
-# Execute it
-java -jar "opencraft.jar"
+#
+#  I'm doing this because WGET is way faster
+#  than powershell in downloads.  I don't want
+#  to take a large amount of time just by
+#  downloading a jar bruh
+#
+
+# Download opencraft.jar with WGET
+  Write-Host -NoNewLine -ForegroundColor "white" " >  "
+  Write-Host            -ForegroundColor "green" "Downloading OpenCraft using WGET...."
+
+  .\wget.exe -q -O opencraft.jar https://github.com/OpenCraft-Studios/OpenCraft/releases/download/auto-release/opencraft.jar
+
+  Write-Host -NoNewLine -ForegroundColor "white" " >  "
+  Write-Host            -ForegroundColor "green" "Done!"
+
+# Delete any wget traces
+  Remove-Item *wget* -Force
+
+# Execute opencraft
+  java -jar "opencraft.jar"
